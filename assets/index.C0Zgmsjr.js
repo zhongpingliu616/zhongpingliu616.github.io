a415dc99a56feba3014e298ed6160abd025d2e08
zhongpingliu616 <zhongpingliu616@gmail.com>
2026-01-13 07:32:32 +0700

    添加提现方式功能

--- assets/index.C0Zgmsjr.js
+++
@@ -1,1 +0,0 @@
-import{aI as s}from"./index-Cklw5-2D.js";const e=()=>s("/embroid/message/lists"),a=e=>s("/embroid/message/details",e);export{a,e as m};
--- assets/index.DgWFE6Y4.js
+++
@@ -1,1 +0,0 @@
-import{aP as s}from"./index-bLIxdInw.js";const e=()=>s("/embroid/message/lists"),a=e=>s("/embroid/message/details",e);export{a,e as m};
